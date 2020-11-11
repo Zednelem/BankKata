ead0f2a736ebe9d001b33bb58f0ffef10953d8dc
Xavier Melendez <reivax.melendez@gmail.com>
2020-11-11 18:57:25 +0100

    task(ftUS1): callback listening corrections

--- src/main/webapp/app/bank/dashboard/dashboard.component.ts
+++ src/main/webapp/app/bank/dashboard/dashboard.component.ts
@@ -1,9 +1,16 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { AccountService } from 'app/core/auth/account.service';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Account } from 'app/core/user/account.model';
-import { Subscription } from 'rxjs';
+import { ReplaySubject, Subscription } from 'rxjs';
 import { BankService } from 'app/core/bank/bank.service';
+
+enum State {
+  INIT = 'INIT',
+  LOADING = 'LOADING',
+  ERROR = 'ERROR',
+  SUCCESS = 'SUCCESS',
+}
 
 @Component({
   selector: 'jhi-dashboard',
@@ -15,34 +22,37 @@ export class DashboardComponent implements OnInit {
   account: Account | null = null;
   authSubscription?: Subscription;
 
-  // depositAmount = new FormControl('');
-
   formGroup: FormGroup;
-  messageToDisplay = 'Message to display';
+
   depositAmountvalue: number;
 
-  constructor(private accountService: AccountService, private formBuilder: FormBuilder, private bankService: BankService) {
+  public States = State;
+  private state: State = State.INIT;
+  public state$: ReplaySubject<State> = new ReplaySubject<State>();
+
+  constructor(public accountService: AccountService, private formBuilder: FormBuilder, private bankService: BankService) {
     this.formGroup = this.formBuilder.group({
-      depositAmount: ['101', [Validators.required, Validators.min(0)]],
+      depositAmount: ['0.00', [Validators.required, Validators.min(0)]],
     });
     this.depositAmountvalue = 0;
+    this.state$.next(State.INIT);
   }
 
   ngOnInit(): void {
     this.authSubscription = this.accountService.getAuthenticationState().subscribe(account => (this.account = account));
   }
 
-  isAuthenticated(): boolean {
-    return this.accountService.isAuthenticated();
+  deposit(): void {
+    this.stateHasChanged(State.LOADING);
+    this.depositAmountvalue = this.formGroup.value.depositAmount;
+    this.bankService.deposeMoney(this.formGroup.value.depositAmount).subscribe(
+      () => this.stateHasChanged(State.SUCCESS),
+      () => this.stateHasChanged(State.ERROR)
+    );
   }
 
-  deposit(): void {
-    const result: number = this.formGroup.value.depositAmount;
-    this.messageToDisplay = 'deposing to the bank ...';
-    this.bankService.deposeMoney(result).subscribe(
-      value => (this.messageToDisplay = `Successfully deposed to the bank transactionId = ${value}`),
-      value => (this.messageToDisplay = `Error: Could not transfert the money: ${JSON.stringify(value)}`)
-    );
-    this.depositAmountvalue = result;
+  stateHasChanged(newState: State): void {
+    this.state = newState;
+    this.state$.next(this.state);
   }
 }
